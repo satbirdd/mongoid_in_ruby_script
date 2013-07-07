06cf0c3eee1b968c0bde637666fbce9effe7627c
liulei <liulei@liulei-NF520-A2G.(none)>
2013-07-07 16:29:55 +0800

    init

---
+++ mongoid.rb
@@ -0,0 +1,11 @@
+require 'mongoid'
+require 'open-uri'
+
+Mongoid.load!('mongoid.yml', :production)
+
+class ImageFile
+	include Mongoid::Document
+	field :imagefile, type: Moped::BSON::Binary
+end
+
+# ImageFile.create(imagefile: Moped::BSON::Binary.new(:generic, open('http://img03.taobaocdn.com/imgextra/i3/323476571/T28SPFXX4XXXXXXXXX_!!323476571.jpg','rb') {|io| io.read }))
